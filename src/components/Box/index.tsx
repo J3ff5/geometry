27837f039288fb542f3ec2d640557aa3194cbcf1
J3ff5 <jeffersonsoaresbh@gmail.com>
2023-05-24 20:22:38 -0300

    :sparkles:

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,8 +1,9 @@
 import React, { useRef } from 'react'
 import { useFrame } from 'react-three-fiber'
 import { Mesh } from 'three'
+import { IProps } from './Box.structure'
 
-export default function Box() {
+export default function Box(props: IProps) {
   const ref = useRef<Mesh>(null)
 
   const [clicked, setClicked] = React.useState(true as boolean)
@@ -21,9 +22,10 @@ export default function Box() {
       onClick={() => {
         setClicked(!clicked)
       }}
+      scale={props.size}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="hotpink" />
+      <meshStandardMaterial color={props.color} />
     </mesh>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import React, { useRef } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import React from 'react'
+import { Canvas } from 'react-three-fiber'
 import Box from '@/components/Box'
-import { Mesh } from 'three'
+import { useControls } from 'leva'
 import { OrbitControls } from '@react-three/drei'
 
 export default function Home() {
+  const { color, size } = useControls({
+    color: '#e03de8',
+    size: {
+      value: 1,
+      min: 0.2,
+      max: 3,
+      step: 0.1
+    }
+  })
   return (
     <div className="flex flex-col w-auto justify-center h-screen ">
       <Canvas>
@@ -12,7 +21,7 @@ export default function Home() {
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
         <pointLight position={[10, 10, 10]} />
-        <Box />
+        <Box color={color} size={size} />
         <OrbitControls />
       </Canvas>
     </div>
